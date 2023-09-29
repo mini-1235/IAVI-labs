94c8f2c030e9671a7490cdb4906e262a860f8d0c
Maurice-1235 <mauricepurnawan@gmail.com>
2023-09-30 02:14:46 +0800

    feat: modify sklearn dataframe

--- sklearn.py
+++ sklearn.py
@@ -1,10 +1,9 @@
+import numpy as np
 from sklearn.linear_model import LinearRegression
-import pandas as pd
 
-
-# Parse the data from the selection into a pandas DataFrame
-data = '''result+Expo_1000000.txt
-
+# Given data
+data = '''
+result+Expo_1000000.txt
 Gain_0.bmp 249.78714718126048
 Gain_4.bmp 253.36180267129842
 Gain_8.bmp 254.47718678290488
@@ -14,7 +13,6 @@ Gain_24.bmp 254.66666666666666
 Gain_16.bmp 254.66666666666666
 
 result+Expo_100000.txt
-
 Gain_0.bmp 184.27959777049568
 Gain_4.bmp 201.44926227846787
 Gain_8.bmp 215.42331527629597
@@ -24,7 +22,6 @@ Gain_20.bmp 248.85570928116903
 Gain_24.bmp 252.35334081652525
 
 result+Expo_1000.txt
-
 Gain_0.bmp 18.923448177043642
 Gain_4.bmp 24.99734396962692
 Gain_8.bmp 31.59241199575776
@@ -34,7 +31,6 @@ Gain_20.bmp 65.14739996126099
 Gain_24.bmp 80.93947452539416
 
 result+Expo_10000.txt
-
 Gain_0.bmp 67.69676342687852
 Gain_4.bmp 86.57251075378076
 Gain_8.bmp 106.37267433614456
@@ -44,7 +40,6 @@ Gain_20.bmp 179.58848272462276
 Gain_24.bmp 196.22343447020694
 
 result+Expo_100.txt
-
 Gain_0.bmp 4.259268851398838
 Gain_4.bmp 5.226738995368254
 Gain_8.bmp 6.592922099125302
@@ -54,7 +49,6 @@ Gain_20.bmp 19.07735035203814
 Gain_24.bmp 25.25906556419245
 
 result+Expo_10.txt
-
 Gain_0.bmp 3.3011939766126437
 Gain_4.bmp 4.074258706222798
 Gain_8.bmp 5.01439555231672
@@ -64,28 +58,39 @@ Gain_20.bmp 14.991095980668597
 Gain_24.bmp 20.288636741308068
 '''
 
-data = data.strip().split('\n\n')
-df = pd.DataFrame([i.split('\n') for i in data], columns=['filename', 'data'])
-df[['exposure', 'gain']] = df['filename'].str.extract(r'Expo_(\d+).txt\n\nGain_(\d+).bmp')
-df['data'] = df['data'].astype(float)
-df['exposure'] = df['exposure'].astype(int)
-df['gain'] = df['gain'].astype(int)
+# Split the data into lines
+lines = data.strip().split('\n')
 
-# Create a linear regression model using scikit-learn
+# Initialize lists to store exposure, gain, and pixel values
+exposure_gain = []
+pixel_values = []
+
+# Parse the data
+current_exposure_gain = None
+for line in lines:
+    if line.startswith('result+Expo_'):
+        # Extract the exposure time from the line
+        exposure = int(line.split('_')[1].split('.')[0])
+        current_exposure_gain = (exposure, 0)  # Initialize gain as 0 dB
+    elif line.startswith('Gain_'):
+        gain = int(line.split('_')[1].split('.')[0])
+        current_exposure_gain = (current_exposure_gain[0], gain)
+        pixel_value = float(line.split()[-1])
+        exposure_gain.append(current_exposure_gain)
+        pixel_values.append(pixel_value)
+
+# Convert lists to NumPy arrays
+exposure_gain = np.array(exposure_gain)
+pixel_values = np.array(pixel_values)
+print(exposure_gain)
+print(pixel_values)
+# Create a linear regression model
 model = LinearRegression()
 
-# Fit the model to the data
-model.fit(df[['exposure', 'gain']], df['data'])
+# Fit the model
+model.fit(exposure_gain, pixel_values)
 
-# Use the model to predict pixel values for a range of exposure and gain values
-exposure_range = range(0, 1000001, 100000)
-gain_range = range(0, 25, 4)
-predictions = []
-for exposure in exposure_range:
-  for gain in gain_range:
-    predictions.append([exposure, gain, model.predict([[exposure, gain]])[0]])
-
-# Convert the predictions to a pandas DataFrame
-predictions_df = pd.DataFrame(predictions, columns=['exposure', 'gain', 'pixel'])
-
-print(predictions_df)
+# Print the coefficients and intercept of the model
+print("Coefficient (exposure):", model.coef_[0])
+print("Coefficient (gain):", model.coef_[1])
+print("Intercept:", model.intercept_)
