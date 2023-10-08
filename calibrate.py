e266c519cf185bbcbf30e4de7ec79c5fc26b8b3a
Maurice-1235 <mauricepurnawan@gmail.com>
2023-10-08 23:23:26 +0800

    feat: add real time calibration

--- calibrate.py
+++ calibrate.py
@@ -4,23 +4,27 @@ import numpy as np
 import cv2
 import glob
 import json
+import sys 
 
 Grid_Point_Col = 8
 Grid_Point_Row = 6
 # Set termination criteria. We stop either when an accuracy is reached or when
 # we have finished a certain number of iterations.
-criteria = cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 40, 0.001
+criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 40, 0.001)
 
 chessboard_points = np.zeros((Grid_Point_Col * Grid_Point_Row, 3), np.float32)
 chessboard_points[:, :2] = np.mgrid[0:Grid_Point_Col, 0:Grid_Point_Row].T.reshape(-1, 2)
 
 image_points = []
 object_points = []
-
+loop_count = 0
 image_paths = glob.glob('dataset/lab3/*.jpg')
 image_size = None
-
+# print(sys.argv[1])
 for image_path in image_paths:
+    print(loop_count)
+    # if(loop_count == sys.argv[1]):
+    #     break
     print(image_path)
     image = cv2.imread(image_path)
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -38,9 +42,11 @@ for image_path in image_paths:
         cv2.imshow('Chessboard Calibration', image)
         cv2.imwrite('processing.jpg', image)
         cv2.waitKey(200)
+        loop_count += 1
 
     else:
         print(image_path)
+        pass
         print("Chessboard Not Detectable!")
 
 cv2.destroyAllWindows()
@@ -65,7 +71,7 @@ for tvec in tvecs:
     tranlsation_matrix,_ =  cv2.Rodrigues(tvec)
     print(tranlsation_matrix)
 
-image_before_undistort = cv2.imread('dataset/lab3/IMG_20231008_144253.jpg')
+image_before_undistort = cv2.imread('dataset/lab3/IMG_20231008_163510.jpg')
 h, w = image_before_undistort.shape[:2]
 new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 image_after_undistort = cv2.undistort(image_before_undistort, mtx, dist, None, new_camera_matrix)
@@ -83,3 +89,4 @@ for i in range(len(object_points)):
 mean_error = total_error / len(object_points)
 print("\n\n------total error:------\n\n", total_error)
 print("\n\n------mean error:------\n\n", mean_error)
+# print(mean_error)
